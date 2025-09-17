0946c4e6f525503fa6ae0cf135cd83a3f8516d65
CLOUDITIN DIGITAL <61991235+cloud-it-in@users.noreply.github.com>
2025-09-17 21:40:44 +0700

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,35 +12,6 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-        ],
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
