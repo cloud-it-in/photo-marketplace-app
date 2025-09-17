7785b19600103bcfb6445328b240ce3a6154463f
CLOUDITIN DIGITAL <61991235+cloud-it-in@users.noreply.github.com>
2025-09-17 22:54:25 +0700

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
-// Updated config file
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: [
-      'amunik-app-2025.s3.amazonaws.com',
-      's3.amazonaws.com',
-      'amazonaws.com'
-    ],
-  },
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
 }
 
 module.exports = nextConfig
