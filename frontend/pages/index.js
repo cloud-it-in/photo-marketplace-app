7520e1ef98341694d55ac0af9be2309bf522b500
CLOUDITIN DIGITAL <61991235+cloud-it-in@users.noreply.github.com>
2025-09-18 14:57:28 +0700

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,16 +1,5 @@
-import Head from 'next/head'
-import PhotoMarketplace from '../components/PhotoMarketplace'
+import PhotoMarketplace from '../components/PhotoMarketplace';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Photo Marketplace - Buy and Sell Unique Photos</title>
-        <meta name="description" content="A marketplace for buying and selling unique photography.Upload your photos and set your price, or browse and purchase amazing photos from other creators. />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <PhotoMarketplace />
-    </>
-  )
+  return <PhotoMarketplace />;
 }
